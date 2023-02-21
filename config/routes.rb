b378b90d4ce81dee0fd0d97cb43766d978cfaf34
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-02-21 19:33:55 +0000

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # List tasks
-  get "tasks", to: "tasks#list"
-  # Create new Task
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  # View task details
-  get "tasks/:id", to: "tasks#find", as: "task"
-  # Edit task
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
-  # Remove task
-  delete "tasks/:id", to: "tasks#destroy"
+  # Read all
+  # get "tasks", to: "tasks#list"
+  # # Create new Task
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # # Read one
+  # get "tasks/:id", to: "tasks#show", as: "task"
+  # # Update task
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+  # # Delete task
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
